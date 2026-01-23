ab1ebbc55116de73a25706e5f371fdfafbc9d5ec
Colin Rotherham <work@colinr.com>
2026-01-23 13:42:29 +0000

    Turn off component cache

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,7 @@ import { type NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
   /* config options here */
+  cacheComponents: false,
   reactStrictMode: true,
   sassOptions: {
     implementation: 'sass-embedded',
